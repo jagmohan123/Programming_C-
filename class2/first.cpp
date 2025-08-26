e049d24c112b6606f80ba2ac9a87198648876dc5
Jagmohan Rai <102997544+jagmohan123@users.noreply.github.com>
2025-08-26 21:40:56 +0530

    class 2 of c++

---
+++ class2/first.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+// 	cout << "Primitive data types" << endl;
+//      //int x1 = 68930905000;// we cross the int range
+//     //cout<<"x value is"<<x1<<endl;
+// 	int x=90;
+// 	// range
+// 	short d=32767;
+// 	cout<<"D value is"<<d<<endl;
+
+// 	float y = 20.7f;
+// 	double num=45.7f;
+// 	bool isStudent = false;
+// 	char FirstAlphabet = 'W';
+// 	string name ="Rohan";
+// 	cout<<endl;
+// 	cout<<"\n";
+// 	cout<<"integer X value is"<<x<<endl;
+// 	cout<<"Float Y value is"<<y<<endl;
+// 	cout<<"Double num value is"<<num<<endl;
+// 	cout<<"Boolean IsStudent value is  "<<isStudent<<endl;
+// 	cout<<"Boolean FirstAlphabet value is "<<FirstAlphabet<<endl;
+// 	cout<<"String Name value is "<<name<<endl;
+
+// 	cout<<"integer X value is"<<endl;
+// 	cout<<"Float Y value is"<<y<<endl;
+// 	cout<<"Double num value is"<<num<<endl;
+// 	cout<<"Boolean IsStudent value is  "<<isStudent<<endl;
+// 	cout<<"Boolean FirstAlphabet value is "<<FirstAlphabet<<endl;
+// 	cout<<"String Name value is "<<name<<endl;
+
+
+	// how to take input to the user
+	// cin keyword ka use i/p lene ke leaye hota hai
+
+// 	int num1;
+// 	float num2;
+
+// 	cout<<"Enter the value of num1";
+// 	cin>>num1;
+
+	// endl use for giving the new line
+
+// 	cout<<"Enter the value of num2";
+// 	cin>>num2;
+// 	cout<<"the sum of num1 and num2"<<num1+num2<<endl;
+// 	int ans=num1+num2;
+// 	cout<<"store sum in ans which is integer type"<<ans<<endl;
+// 	float x1=10, x2=20;
+
+// 	cout<<x1+x2<<endl;
+
+
+// Operator-> its kind of symbol which is used to perform some operation in operand(value)
+//Arithmetic->
+	cout<<"###########################Arithmetic##################################"<<endl;
+	cout<<10+20<<endl;
+	cout<<100-20<<endl;
+	cout<<100/20<<endl;
+	cout<<10*20<<endl;
+	cout<<10%20<<endl;
+
+// comparision/Relational Operator-
+	cout<<"###########################Comparision/ Relational##################################"<<endl;
+	cout<<(10<20)<<endl;
+	cout<<(100>20)<<endl;
+	cout<<(100>=20)<<endl;
+	cout<<(10<=20)<<endl;
+	cout<<(10!=20)<<endl;
+
+
+
+// comparision/Relational Operator-
+	cout<<"########################### Logical Operator-##################################"<<endl;
+	cout<<((10<8)&&(10<20))<<endl;// ek bhi false sab false
+	cout<<((3>5)||(100>20))<<endl; // ek bhi true sab true , if 1st condition wrong check second condition
+	cout<<(!true)<<endl;// reverse of anything
+
+// address of Operator-
+
+
+cout<<"########################### Address of  Operator-##################################"<<endl;
+int value1=890;
+cout<<value1<<endl;
+cout<<"Address of value1 varible"<<&value1<<endl;
+
+
+cout<<"########################### Bitwise Operator-##################################"<<endl;
+int value1=890;
+cout<<value1<<endl;
+cout<<"Address of value1 varible"<<&value1<<endl;
+
+
+
+
+
+
+
+	return 0;
+
+
+}
